b80956b43cdbaf0add1863dc63fa75d4eefec214
Matthieu NICOLAS <mtthncls@gmail.com>
2019-04-18 15:17:37 +0200

    call eggs and characters APIs with console.log to check success

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
 class App extends Component {
+  componentDidMount() {
+    fetch('http://easteregg.wildcodeschool.fr/api/eggs')
+      .then(eggsFromAPI => eggsFromAPI.json())
+      .then(eggsData => {
+        console.log(eggsData[0].name);
+      }
+      );
+
+    fetch('http://easteregg.wildcodeschool.fr/api/characters')
+      .then(charsFromAPI => charsFromAPI.json())
+      .then(charsData => {
+        console.log(charsData[0].name);
+      }
+      );
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
