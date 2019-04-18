a54272e4b2c40551a7fe8f19942fd4efacd74142
Greg <gregoire.suray@gmail.com>
2019-04-19 00:24:40 +0200

    display both characters to select one

---
+++ src/Components/CharacterChoice.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+const CharacterChoice = ({IAName, IAHitpoints, hitMethod}) => {
+    return (
+        <section className='card text-white bg-dark mb-3'>
+            <Container>
+                <Row>
+                    <div className='picture'><img src={IAName.image} className="card-img-top" alt={IAName.name}/></div>
+                    <p>{IAName.name}</p>  
+                    <Col><img src="https://i.pinimg.com/originals/d3/65/d8/d365d88fb1a2d967df2efce34f80fbce.png" className="life" alt="life"/></Col>
+                    <Col><p>{IAHitpoints}</p></Col>
+                    <button onClick={hitMethod}>Hit the opponent !</button>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+
+export default CharacterChoice
