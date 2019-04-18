8a442ca1251b514c599ff965c22b50ca3ca2ca7a
Matthieu NICOLAS <mtthncls@gmail.com>
2019-04-18 15:56:11 +0200

    randomized and numbered characters and eggs api call

--- src/App.js
+++ src/App.js
@@ -10,17 +10,47 @@ class App extends Component {
     fetch('http://easteregg.wildcodeschool.fr/api/eggs')
       .then(eggsFromAPI => eggsFromAPI.json())
       .then(eggsData => {
-        console.log(eggsData[0].name);
+        // console.log(eggsData[0].name);
+        let eggsToRandomize = this.randomizeAPIDisplay(eggsData);
+        // console.log(eggsToRandomize[0].name);
+        let eggsToDisplay = this.selectNumberOfDisplay(eggsToRandomize, 30)
+        console.log(eggsToDisplay)
+
       }
       );
 
     fetch('http://easteregg.wildcodeschool.fr/api/characters')
       .then(charsFromAPI => charsFromAPI.json())
       .then(charsData => {
-        console.log(charsData[0].name);
+        // console.log(charsData[0].name);
+        let charsToRandomize = this.randomizeAPIDisplay(charsData);
+        // console.log(charsToRandomize[0].name);
+        let charsToDisplay = this.selectNumberOfDisplay(charsToRandomize, 10)
+        console.log(charsToDisplay)
       }
       );
   };
+  randomizeAPIDisplay = (apiResponse) => {
+    let currentIndex = apiResponse.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = apiResponse[currentIndex];
+      apiResponse[currentIndex] = apiResponse[randomIndex];
+      apiResponse[randomIndex] = temporaryValue;
+    };
+    return apiResponse
+  };
+
+  selectNumberOfDisplay = (apiRandomizedResponse, displayNumber) => {
+    let selectedDisplays = []
+    for (let i = 0; i < displayNumber; i++) {
+      selectedDisplays.push(apiRandomizedResponse[i])
+    }
+    return selectedDisplays
+  }
   render() {
     return (
       <div className="App">
