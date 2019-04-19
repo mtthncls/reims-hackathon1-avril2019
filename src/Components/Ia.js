cca69b917d625694aaa688b915ab1cbefee3a92f
Chandelier Axel <AxeletCana@hotmail.fr>
2019-04-19 09:53:22 +0200

    Deleted button of IA

--- src/Components/Ia.js
+++ src/Components/Ia.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
-const Ia = ({hitMethod, IAName, IAHitpoints, selectedEggs}) => {
+const Ia = ({hitMethod, IAName, IAHitpoints, selectedEggs, displayNoneButton}) => {
     return ( 
         <section className='card text-white bg-dark mb-3'>
             <Container>
@@ -9,7 +9,7 @@ const Ia = ({hitMethod, IAName, IAHitpoints, selectedEggs}) => {
                     <p>{IAName.name}</p>  
                     <Col><img src="https://i.pinimg.com/originals/d3/65/d8/d365d88fb1a2d967df2efce34f80fbce.png" className="life" alt="life"/></Col>
                     <Col><p>{IAHitpoints}</p></Col>
-                    {selectedEggs.length > 0 && <Button onClick={hitMethod}>Hit the opponent !</Button>}
+                    {displayNoneButton && selectedEggs.length > 0 && <Button onClick={hitMethod}>Hit the opponent !</Button>}
                 </Row>
             </Container>
         </section>
