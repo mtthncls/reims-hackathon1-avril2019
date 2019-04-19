9bddceee910dab944ec31cfaec3968fb239976a9
Sébastien Jolly <seb_879@hotmail.fr>
2019-04-19 10:52:37 +0200

    add Try again Button

--- src/App.js
+++ src/App.js
@@ -110,6 +110,16 @@ class App extends Component {
     !this.state.isCharacterSelected && this.setState({ isSelectCharacterIA: this.state.charactersRandomizedFromApi[3] })
   }
 
+  isTryAgainDisplay = () => {
+    this.setState({isBattlefieldDisplayed: false,})
+    this.setState({isCharacterSelected: false,})
+    this.setState({areEggsSelected: false,})
+    this.setState({isSelectedCharacter: []})
+    this.setState({isSelectedCharacterIA: []})
+    this.setState({IAHitpoints: "20",})
+    this.setState({userHitpoints: "20",})
+  }
+
 
   render() {
     return (
@@ -165,11 +175,11 @@ class App extends Component {
               </div>
             </Col>
             
-            <Col offset={4} />
+            <Col offset={4}> {this.state.areEggsSelected === true && this.state.selectedEggs.length === 0 && <Button onClick={() => this.isTryAgainDisplay()} className="btnTryAgain" size="lg" color="warning">Try Again</Button>}</Col>
 
             <Col xs={2}>
               <div className = "omagad">{this.state.selectedEggs.length === 0 && this.state.selectedEggsIA.length === 0
-                  ? <p>Nice</p> 
+                  ? <p></p> 
                   : this.state.selectedEggsIA.length === 0 
                   ? <p className="noMoreEggs"></p> 
                   : this.state.selectedEggsIA.map(egg => <DisplayEgg isBattlefieldDisplayed={this.state.isBattlefieldDisplayed} 
@@ -186,7 +196,7 @@ class App extends Component {
           </Row>
         </Container>
         }
-
+        
 
       </div>
     );
