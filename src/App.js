30757d59bbf622fee773464e6f15fc9f2cc31ce0
Chandelier Axel <AxeletCana@hotmail.fr>
2019-04-19 09:41:23 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import DisplayEgg from "./DisplayEgg";
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
 import './App.css';
 import Ia from './Components/Ia';
 import CharacterChoice from './Components/CharacterChoice';
@@ -183,7 +183,6 @@ class App extends Component {
                       IAName={this.state.isSelectCharacterIA}/>}
               </div>
             </Col>
-
           </Row>
         </Container>
         }
