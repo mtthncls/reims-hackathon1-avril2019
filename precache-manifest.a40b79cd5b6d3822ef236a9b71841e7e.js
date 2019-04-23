460d890f842507810a60afa35ad002bded10401d
Matthieu NICOLAS <mtthncls@gmail.com>
2019-04-23 13:56:40 +0200

    Updates

---
+++ precache-manifest.a40b79cd5b6d3822ef236a9b71841e7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7e2d2fd61b712d2a32c",
+    "url": "/mtthncls/reims-hackathon1-avril2019/static/js/runtime~main.8ff1aea5.js"
+  },
+  {
+    "revision": "ef6bf310c2a6ee0e6fb5",
+    "url": "/mtthncls/reims-hackathon1-avril2019/static/js/main.57c2b811.chunk.js"
+  },
+  {
+    "revision": "e727b77ffc43d7f926fc",
+    "url": "/mtthncls/reims-hackathon1-avril2019/static/js/2.f493a4c1.chunk.js"
+  },
+  {
+    "revision": "ef6bf310c2a6ee0e6fb5",
+    "url": "/mtthncls/reims-hackathon1-avril2019/static/css/main.c010bf80.chunk.css"
+  },
+  {
+    "revision": "e727b77ffc43d7f926fc",
+    "url": "/mtthncls/reims-hackathon1-avril2019/static/css/2.cf842d24.chunk.css"
+  },
+  {
+    "revision": "2aec0d0d92094050c2456cdba08db590",
+    "url": "/mtthncls/reims-hackathon1-avril2019/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mtthncls/reims-hackathon1-avril2019/precache-manifest.a40b79cd5b6d3822ef236a9b71841e7e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mtthncls/reims-hackathon1-avril2019/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
